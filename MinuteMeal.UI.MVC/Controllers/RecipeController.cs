806faafdc43073b6088c28a7be24a56bcb46dbc1
despeghelkobe <kobe.despeghel@student.vives.be>
2025-03-10 16:35:46 +0100

    feat: table view for recipe list on different controller

---
+++ MinuteMeal.UI.MVC/Controllers/RecipeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinuteMeal.UI.MVC.Core;
+
+namespace MinuteMeal.UI.MVC.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private readonly MinuteMealDatabase _database;
+
+        public RecipeController(MinuteMealDatabase database)
+        { 
+            _database = database;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View(_database.Recipes);
+        }
+    }
+}
